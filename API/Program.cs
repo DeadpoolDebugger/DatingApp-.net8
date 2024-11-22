d13d1e9e21d107dc67edac23501a6ba5063a98f6
DeadpoolDebugger <together.we.learn.new@gmail.com>
2024-11-22 14:37:33 +0530

    WK2 AngJS Building Skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -14,5 +15,6 @@ builder.Services.AddDbContext<DataContext>(opt =>
 var app = builder.Build();
 
 app.MapControllers();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.Run();
